6a82fdf7a58498935979a9c180815ff3eaea76a9
Slainez <sullyvan360@gmail.com>
2022-07-26 10:57:26 +0200

    ajout exercice distributeur etape 1

---
+++ src/main/java/fr/m2i/webapplication/DistributeurServlet.java
@@ -0,0 +1,82 @@
+
+package fr.m2i.webapplication;
+
+import fr.m2i.webapplication.distributeur.Distributeur;
+import fr.m2i.webapplication.distributeur.Produit;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author moi
+ */
+public class DistributeurServlet extends HttpServlet {
+
+    Distributeur distri = Distributeur.getInstance();
+    int credit = distri.getCredit();
+    List<Produit> productList = distri.getStock();
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.setAttribute("credit",credit);
+        request.setAttribute("stockList",productList);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/distributeurWeb.jsp").forward(request,response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acheterProduit = request.getParameter("acheterProduit");
+        String addCredit = request.getParameter("ajoutCredit");
+        
+        if(acheterProduit != null){
+            distri.commanderProduit(Integer.getInteger(acheterProduit));
+        }
+        if(addCredit != null){
+            int montant = 0 ;
+            switch(addCredit){
+                case "increaseOne": 
+                    montant = 1 ;
+                    break;
+                case "increaseTwo" :
+                    montant = 2 ;
+                    break;
+                default :
+                    montant = 0 ;                  
+            }                    
+            distri.insererArgent(montant);
+            request.setAttribute("credit",credit );
+        }
+        
+        
+       
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/WEB-INF/distributeurWeb.jsp
@@ -0,0 +1,54 @@
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <jsp:include page="../include/header.jsp" ><jsp:param name="title" value="Distributeur Web" /></jsp:include>
+   
+    <body>
+        <h1>Distributeur Web</h1>
+        
+        <h1>Crédit : <c:out value="${credit}" /></h1>
+    
+        <h2>Liste des produits</h2>
+        <table>
+            <tr>
+                <th>Id Produit</th>
+                <th>Nom</th>
+                <th>Quantite</th>
+                <th>Prix</th>
+            </tr>
+         <c:forEach items="${stockList}" var="produit" >
+            <tr>
+                <td>
+                    <c:out value="${produit.id}" /> 
+                </td>
+                <td>
+                    <c:out value="${produit.nom}" /> 
+                </td>
+                <td>
+                    <c:out value="${produit.quantite}" /> 
+                </td>
+                <td>
+                    <c:out value="${produit.prix}" /> 
+                </td>
+                <td> 
+                    <form method="POST" name="acheterProduit">
+                        <input type="submit" value="Acheter" name="${produit.id}" >            
+                    </form>
+                </td>
+            </tr>            
+        </c:forEach>            
+        </table>
+        
+       
+        
+        
+        <form method="POST" name="ajoutCredit">
+            <input type="submit" value="Ajouter 1 credit" name="increaseOne">
+            <input type="submit" value="Ajouter 2 credit" name="increaseTwo">
+        </form>
+        
+        
+    </body>
+</html>
